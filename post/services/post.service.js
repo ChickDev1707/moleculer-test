b70109ebe168825afafa85eba9b7b2179a2d7589
chickdev1707 <thuancoixy234786@gmail.com>
2023-01-09 23:42:35 +0700

    init project

---
+++ post/services/post.service.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/**
+ * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+/** @type {ServiceSchema} */
+module.exports = {
+	name: "posts",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		getPosts: {
+			rest: {
+				method: "GET",
+				path: "/"
+			},
+			async handler() {
+				return "All posts";
+			}
+		},
+	},
+};
---
+++ user/services/user.service.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+/** @type {ServiceSchema} */
+module.exports = {
+	name: "users",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		getUsers: {
+			rest: {
+				method: "GET",
+				path: "/"
+			},
+			async handler() {
+				return "all users";
+			}
+		},
+	},
+
+};
